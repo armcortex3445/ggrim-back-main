aaa204f665f05209c7f84a26071fc4211929bb8a
armcortex3445 <wlghks5966@naver.com>
2024-11-07 18:15:28 +0900

    test(PaintingModule) add unit test

--- src/painting/painting.controller.spec.ts
+++ src/painting/painting.controller.spec.ts
@@ -1,17 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaintingController } from './painting.controller';
 import { PaintingService } from './painting.service';
+import { Painting } from './entities/painting.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MockRepository } from '../../test/_shared/mock/mock.repository';
 
 describe('PaintingController', () => {
   let controller: PaintingController;
+  let service: PaintingService;
+  let paintingRepository: MockRepository<Painting> = new MockRepository<Painting>();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PaintingController],
-      providers: [PaintingService],
+      providers: [
+        PaintingService,
+        { provide: getRepositoryToken(Painting), useValue: paintingRepository },
+      ],
     }).compile();
 
     controller = module.get<PaintingController>(PaintingController);
+    service = module.get<PaintingService>(PaintingService);
+    paintingRepository = module.get<MockRepository<Painting>>(getRepositoryToken(Painting));
   });
 
   it('should be defined', () => {
--- src/painting/painting.service.spec.ts
+++ src/painting/painting.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaintingService } from './painting.service';
+import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
+import { Painting } from './entities/painting.entity';
+import { WikiArtPainting } from './entities/wikiArt-painting.entity';
+import { MockRepository } from '../../test/_shared/mock/mock.repository';
+import { TypeormConfig } from '../utils/typeorm.config';
+import { DataSource, DataSourceOptions, Repository, createConnections } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { CreatePaintingDto } from './dto/create-painting.dto';
+import { createConnection } from 'net';
 
 describe('PaintingService', () => {
   let service: PaintingService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PaintingService],
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env.test.local',
+        }),
+      ],
+      providers: [
+        PaintingService,
+        {
+          provide: getRepositoryToken(Painting),
+          useValue: new MockRepository<Painting>(),
+        },
+      ],
     }).compile();
 
     service = module.get<PaintingService>(PaintingService);
