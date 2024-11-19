5e2ea5ea6242cdc2149e326929764811cf25d9e8
armcortex3445 <wlghks5966@naver.com>
2024-11-19 17:59:17 +0900

    doc(test) add testModule description

--- test/_shared/test.module.ts
+++ test/_shared/test.module.ts
@@ -7,6 +7,17 @@ import { TestingModule } from '@nestjs/testing';
 import { LoggerModule } from '../../src/Logger/logger.module';
 import { DataBaseModule } from '../../src/db/db.module';
 
+/* TestModule
+## Purpose:
+- Initialize the test environment for E2E and integration tests.
+- Provide a test API for external modules.
+- Create a DataSource connected to the test database tables.
+
+## Configuration Details:
+- The configuration is defined in the `setEnvVars.js` file.
+- Adapts the module specifically for the test environment.
+*/
+
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
