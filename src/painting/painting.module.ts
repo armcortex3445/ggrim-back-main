678caa7855f5719b4c06f637322257d4e6a8f950
armcortex3445 <wlghks5966@naver.com>
2024-11-21 17:55:32 +0900

    feat(PaintingModule) add WikiArtPaintingService class
    - provide function related to WikiArtPainting Entity

--- src/painting/painting.module.ts
+++ src/painting/painting.module.ts
@@ -4,11 +4,12 @@ import { PaintingController } from './painting.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Painting } from './entities/painting.entity';
 import { WikiArtPainting } from './entities/wikiArt-painting.entity';
+import { WikiArtPaintingService } from './sub-service/wikiArt.painting.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Painting, WikiArtPainting])],
   controllers: [PaintingController],
-  providers: [PaintingService],
-  exports: [PaintingService],
+  providers: [PaintingService, WikiArtPaintingService],
+  exports: [PaintingService, WikiArtPaintingService],
 })
 export class PaintingModule {}
