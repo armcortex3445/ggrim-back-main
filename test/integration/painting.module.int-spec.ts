25bacae3d309050764313f328f41d9fca91664dc
armcortex3445 <wlghks5966@naver.com>
2024-11-26 15:40:17 +0900

    style(PaintingModule) : organize import statement

--- test/integration/painting.module.int-spec.ts
+++ test/integration/painting.module.int-spec.ts
@@ -1,18 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PaintingService } from '../../src/painting/painting.service';
-import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
-import { Painting } from '../../src/painting/entities/painting.entity';
-import { WikiArtPainting } from '../../src/painting/entities/wikiArt-painting.entity';
-import { MockRepository } from '../_shared/mock/mock.repository';
-import { TypeormConfig } from '../../src/utils/typeorm.config';
-import { DataSource, DataSourceOptions, Repository, createConnections } from 'typeorm';
-import { ConfigModule } from '@nestjs/config';
 import { CreatePaintingDto } from '../../src/painting/dto/create-painting.dto';
-import { createConnection } from 'net';
 import { PaintingModule } from '../../src/painting/painting.module';
-import { TestService } from '../_shared/test.service';
+import { PaintingService } from '../../src/painting/painting.service';
 import { TestModule } from '../_shared/test.module';
-import { SearchPaintingDTO } from '../../src/painting/dto/search-painting.dto';
+import { TestService } from '../_shared/test.service';
 
 describe('PaintingModule Integration Test', () => {
   let module: TestingModule;
