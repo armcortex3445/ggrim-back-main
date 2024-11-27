dc020784bb73a6ec66c261f0eaa825c7f8afc08a
armcortex3445 <wlghks5966@naver.com>
2024-11-27 18:46:04 +0900

    feat(PaintingModule) : update Painting Entity
    - do step of normalizing DB

--- src/painting/entities/painting.entity.ts
+++ src/painting/entities/painting.entity.ts
@@ -1,6 +1,15 @@
-import { IsString } from 'class-validator';
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNumber, IsString } from 'class-validator';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { CustomBaseEntity } from '../../db/entity/custom.base.entity';
+import { Tag } from './tag.entity';
 import { WikiArtPainting } from './wikiArt-painting.entity';
 
 @Entity()
@@ -19,4 +28,30 @@ export class Painting extends CustomBaseEntity {
   })
   @JoinColumn()
   wikiArtPainting!: WikiArtPainting;
+
+  @Column({
+    nullable: true,
+  })
+  @IsString()
+  image_url!: string;
+
+  @Column({ type: 'text', default: '' })
+  @IsString()
+  description!: string; // painting description, default: ""
+
+  @Column({ nullable: true })
+  @IsNumber()
+  completition_year!: number; // painting completition year, default: null
+
+  @Column({ nullable: true })
+  @IsNumber()
+  width!: number;
+
+  @Column({ nullable: true })
+  @IsNumber()
+  height!: number;
+
+  @ManyToMany(() => Tag, (photo) => photo.paintings)
+  @JoinTable()
+  tags!: Tag[];
 }
