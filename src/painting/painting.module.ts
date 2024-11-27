0cae3aa30b709177f49945d99fbe98c625acea02
armcortex3445 <wlghks5966@naver.com>
2024-11-27 18:45:07 +0900

    feat(PaintingModule) : add Tag Entity
    - do setop of nomalizing DB

--- src/painting/painting.module.ts
+++ src/painting/painting.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PaintingService } from './painting.service';
-import { PaintingController } from './painting.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Painting } from './entities/painting.entity';
+import { Tag } from './entities/tag.entity';
 import { WikiArtPainting } from './entities/wikiArt-painting.entity';
+import { PaintingController } from './painting.controller';
+import { PaintingService } from './painting.service';
 import { WikiArtPaintingService } from './sub-service/wikiArt.painting.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Painting, WikiArtPainting])],
+  imports: [TypeOrmModule.forFeature([Painting, WikiArtPainting, Tag])],
   controllers: [PaintingController],
   providers: [PaintingService, WikiArtPaintingService],
   exports: [PaintingService, WikiArtPaintingService],
