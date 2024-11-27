3792ffdf48cf1553b613153b991a599473974eaf
armcortex3445 <wlghks5966@naver.com>
2024-11-27 15:50:39 +0900

    feat(DBModule) : add CustomBaseEntity class
    - add common columns to log table row

--- src/painting/entities/painting.entity.ts
+++ src/painting/entities/painting.entity.ts
@@ -1,9 +1,10 @@
 import { IsString } from 'class-validator';
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { CustomBaseEntity } from '../../db/entity/custom.base.entity';
 import { WikiArtPainting } from './wikiArt-painting.entity';
 
 @Entity()
-export class Painting {
+export class Painting extends CustomBaseEntity {
   @PrimaryGeneratedColumn('uuid')
   @IsString()
   id!: string;
