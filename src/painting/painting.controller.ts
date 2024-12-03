8d40060884e4bea9da06b4a9e79d070c481efb38
armcortex3445 <wlghks5966@naver.com>
2024-12-03 15:49:11 +0900

    refactor(PaintingModule) : refactor route handler
    - config base route of CrudCOntroller to restrict use it
    - delete wiki-art route handler to synchornize with normalized Painting
      Table

--- src/painting/painting.controller.ts
+++ src/painting/painting.controller.ts
@@ -7,23 +7,40 @@ import {
   Logger,
   Param,
   ParseIntPipe,
-  Patch,
   Post,
   Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreatePaintingDTO } from './dto/create-painting.dto';
-import { FindPaintingDTO } from './dto/find-painting.dto';
 import { SearchPaintingDTO } from './dto/search-painting.dto';
-import { UpdateWikiArtInfoDTO } from './dto/update-wikiArt-info.dto';
 import { Painting } from './entities/painting.entity';
-import { WikiArtPainting } from './entities/wikiArt-painting.entity';
-import { IResult, PaintingService } from './painting.service';
+import { PaintingService } from './painting.service';
+import { IPaginationResult } from './responseDTO';
 
 @Crud({
   model: {
     type: Painting,
+  },
+  routes: {
+    only: ['getOneBase'],
+  },
+  params: {
+    id: {
+      field: 'id',
+      type: 'uuid',
+      primary: true,
+    },
+  },
+  query: {
+    join: {
+      artist: {
+        eager: true,
+      },
+      tags: {
+        eager: true,
+      },
+    },
   },
 })
 @UsePipes(new ValidationPipe({ transform: true }))
@@ -31,7 +48,7 @@ import { IResult, PaintingService } from './painting.service';
 export class PaintingController implements CrudController<Painting> {
   constructor(public service: PaintingService) {}
 
-  @Get('search')
+  @Get('/')
   async searchPainting(
     @Query() dto: SearchPaintingDTO,
     @Query('page', new DefaultValuePipe(0), ParseIntPipe) page: number,
@@ -49,46 +66,16 @@ export class PaintingController implements CrudController<Painting> {
     return ret;
   }
 
-  @Get('search/:key')
-  async searchPaintingWithKey(
-    @Param('key') key: string,
-    @Query('exclusion') exclusion: string,
-    @Query('inclusion') inclusion: string,
-  ) {
-    return this.service.searchPaintingWithoutAndWithValue(
-      key as keyof WikiArtPainting,
-      JSON.parse(exclusion) as string[],
-      JSON.parse(inclusion) as string[],
-    );
-  }
+  @Get('values/:columnName')
+  async getColumnValues(@Param('columnName') columnName: string) {
+    const map = await this.service.getColumnValueMap(columnName as keyof Painting);
 
-  @Get()
-  async findPainting(@Query() query: FindPaintingDTO) {
-    if (query.id) {
-      Logger.debug('find entity with id :' + query.id);
-      const ret: IResult<Painting> = {
-        data: await this.service.findOneById(query.id),
-      };
-
-      return ret;
-    }
-    return this.service.findPainting(query.wikiArtID);
+    return [...map.values()];
   }
 
   @Post()
   createPainting(@Body() body: CreatePaintingDTO) {
     Logger.debug(`[createPainting] ${JSON.stringify(body)}`);
     return this.service.create(body);
-  }
-
-  //related to wiki-art
-  @Get(':id/wiki-art')
-  async findWikiArtInfo(@Param('id') id: string) {
-    return this.service.getWikiArtInfo(id);
-  }
-
-  @Patch(':id/wiki-art')
-  updateWikiArtInfo(@Param('id') id: string, @Body() updateWikiArtInfo: UpdateWikiArtInfoDTO) {
-    return this.service.updateWikiArt(id, updateWikiArtInfo);
   }
 }
