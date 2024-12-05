47100a5d6e0f30f73089c9e9b6f435fc6f2b4c46
armcortex3445 <wlghks5966@naver.com>
2024-12-05 15:07:50 +0900

    feat(utils) : add function

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -6,6 +6,9 @@ export function isNotFalsy<T>(obj: T | null | undefined): obj is T {
   return obj !== null && obj !== undefined;
 }
 
+export function isFalsy<T>(obj: T | null | undefined): obj is undefined | null {
+  return !isNotFalsy(obj);
+}
 export function isArrayEmpty<T>(list: T[]): boolean {
   return list.length === 0;
 }
