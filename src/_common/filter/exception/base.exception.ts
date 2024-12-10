909ddbb361e0cf0912b24005580c0878ba103e11
armcortex3445 <83511801+armcortex3445@users.noreply.github.com>
2024-12-10 17:16:14 +0900

    Feat warning response (#9)
    * feat(CommonModule) : include exception message in response
    
    * refactor(CommonModule) : refactor CustomExceptionFilter
    
    - follow code convention of naming interface
    - shortcut long function code
    
    * refactor(CommonModule) : refactor BaseException
    
    - follow code convention of naming interface
    - delete duplicated property
    - make clear variable name

--- src/_common/filter/exception/base.exception.ts
+++ src/_common/filter/exception/base.exception.ts
@@ -2,16 +2,15 @@ import { HttpStatus } from '@nestjs/common';
 import { HttpException, HttpExceptionOptions } from '@nestjs/common/exceptions';
 import { TYPE_DEFAULT_VALUE } from '../../const/default.value';
 
-interface IBaseException {
+interface BaseExceptionInterface {
   errorCode: number;
   timestamp: string;
-  msg: string;
   path: string;
 }
 
 export type HttpStatusType = keyof typeof HttpStatus;
 
-export class BaseException extends HttpException implements IBaseException {
+export class BaseException extends HttpException implements BaseExceptionInterface {
   traceId: string = TYPE_DEFAULT_VALUE.string;
   constructor(
     errorCode: number,
@@ -19,20 +18,14 @@ export class BaseException extends HttpException implements IBaseException {
     response: Record<string, any> | string,
     options?: HttpExceptionOptions,
   ) {
-    let resbody = typeof response === 'string' ? { message: response } : response;
-
-    resbody['errorCode'] = errorCode;
-    resbody['status'] = status;
-    super(resbody, status, options);
+    let responseBody = typeof response === 'string' ? { message: response } : response;
+    super(responseBody, status, options);
     this.errorCode = errorCode;
-    this.msg = JSON.stringify(resbody, null, 2);
   }
 
   errorCode: number;
 
   timestamp: string = TYPE_DEFAULT_VALUE.string;
-
-  msg: string;
 
   path: string = TYPE_DEFAULT_VALUE.string;
 }
