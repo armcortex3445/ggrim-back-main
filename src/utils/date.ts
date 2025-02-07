d43efff25ee9924521cf669bd5f09fc84e06a0ea
armcortex3445 <wlghks5966@naver.com>
2025-02-07 16:42:22 +0900

    style(utils) : format code style

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,12 +1,12 @@
 import * as moment from 'moment';
 
 export function getLatestMonday() {
-    const today = moment();
-    const isMonday = today.isoWeekday() === 1; // 월요일인지 확인 (1 = 월요일)
+  const today = moment();
+  const isMonday = today.isoWeekday() === 1; // 월요일인지 확인 (1 = 월요일)
 
-    const latestMonday = isMonday
-      ? today.format('YYMMDD') // 오늘이 월요일이라면 오늘 날짜 사용
-      : today.startOf('isoWeek').format('YYMMDD'); // 오늘이 월요일이 아니면 이번 주 월요일
+  const latestMonday = isMonday
+    ? today.format('YYMMDD') // 오늘이 월요일이라면 오늘 날짜 사용
+    : today.startOf('isoWeek').format('YYMMDD'); // 오늘이 월요일이 아니면 이번 주 월요일
 
-    return latestMonday;
-  }
+  return latestMonday;
+}
